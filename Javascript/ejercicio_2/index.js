c2f97f3300160e4626bbe9190667890ea759f552
Quique <65765175+Enroor@users.noreply.github.com>
2022-07-18 13:08:32 +0200

    Add files via upload

---
+++ Javascript/ejercicio_2/index.js
@@ -0,0 +1,12 @@
+const lista = new Array("Enrique", 28, true);
+
+lista.fecha_nac = new Date(1994, 5, 8);
+
+lista.libro_fav = {
+    titulo: "Harry Potter",
+    autor: "J.K Rowling",
+    fecha: 1997,
+    url: "https://es.wikipedia.org/wiki/Harry_Potter_y_la_piedra_filosofal"
+}
+
+console.log(lista);
