e912ef38c46c972d405a94c53a20a204c8e28b63
Quique <65765175+Enroor@users.noreply.github.com>
2022-07-18 18:03:35 +0200

    Add files via upload

---
+++ Javascript/ejercicio_5/index.js
@@ -0,0 +1,10 @@
+let alt_cm = 173;
+let alt_m = 1.73;
+let peso = 64.1;
+
+let alt_round = Math.ceil(alt_m);
+let peso_round = Math.floor(peso);
+
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(iguales);
