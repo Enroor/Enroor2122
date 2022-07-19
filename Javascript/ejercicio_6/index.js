93d348739e367535ad28aa4135546c29ba98272a
Quique <65765175+Enroor@users.noreply.github.com>
2022-07-19 18:06:09 +0200

    Add files via upload

--- Javascript/ejercicio_6/index.js
+++ Javascript/ejercicio_6/index.js
@@ -3,13 +3,13 @@ listaCompra.push("Aceite de Girasol");
 listaCompra.splice(7, 1);
 
 const listaObj = [
-    {titulo: "Origen", director: 'Christopher Nolan', fecha: "2010"},
-    {titulo: "Origen", director: 'Christopher Nolan', fecha: "2000"},
-    {titulo: "Origen", director: 'Christopher Nolan', fecha: "2015"}
+    {titulo: "Origen", director: 'Christopher Nolan', fecha: new Date(2010, 0, 1)},
+    {titulo: "Origen", director: 'Christopher Nolan', fecha: new Date(2000, 6, 11)},
+    {titulo: "Origen", director: 'Christopher Nolan', fecha: new Date(2015, 5, 25)}
 ];
 
-const fecha = listaObj.filter(valor => valor.fecha > 2010);
+const fecha = listaObj.filter(valor => valor.fecha > new Date(2010, 0, 1));
 const directores = listaObj.map((valor) => valor.director);
 const titulos = listaObj.map((valor) => valor.titulo);
 const newList = directores.concat(titulos);
-const newListProp = [...directores,...titulos];
+const newListProp = [...directores, ...titulos];
