1b73a987a0cd18702765f18a5e40696f37f82494
Quique <65765175+Enroor@users.noreply.github.com>
2022-07-22 19:51:50 +0200

    Add files via upload

---
+++ Javascript/ejercicio_12/index.js
@@ -0,0 +1,16 @@
+function fibonacci(num){
+    if(num === 1){
+        return [1];
+    }
+    if(num === 2){
+        return [1, 1];
+    }
+    let lista = [1, 1];
+
+    for(let i = 2; i < num; i++){
+        lista = [...lista, lista[i -1 ] + lista[i - 2]];
+    }
+    return lista;
+}
+
+console.log(fibonacci(6));
