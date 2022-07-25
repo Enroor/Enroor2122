49ff01b336d283fd4c1b9c44d5842ac810b29690
Quique <65765175+Enroor@users.noreply.github.com>
2022-07-25 12:24:09 +0200

    Update index.js

--- Javascript/ejercicio_14/index.js
+++ Javascript/ejercicio_14/index.js
@@ -3,3 +3,7 @@ const btn = document.querySelector("button");
 btn.addEventListener("click", () =>{
     alert("Click en el botón")
 })
+
+$("button").click(function() {
+    console.log("Hola, estoy utilizando JQuery")
+})
